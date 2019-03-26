e2dd41242c38f5805e43859c44a68595f98ac7d1
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2019-03-26 22:39:37 +1100

    Add data/make_dataset.py

---
+++ src/data/make_dataset.py
@@ -0,0 +1,65 @@
+import pandas_datareader as pdr
+import pandas as pd
+import numpy as np
+
+from datetime import datetime, timedelta
+
+def get_data(stocks, metrics=None, source='yahoo', start='2016-01-01', end=None):
+    """
+
+    :param stocks: list
+    :param metrics: list, optional
+    :param source: str., optional
+    :param start: str., optional
+    :param end: str., optional
+    :return:
+    """
+
+    default_metrics = ["High", "Low", "Open", "Close", "Adj Close", "Volume"]
+
+    if not metrics:
+        metrics = ["Close"]
+
+    elif (len(set(default_metrics) - set(metrics)) == len(set(default_metrics))):
+        msg = "Expected metrics to be in {0}. Got {1}.".format(default_metrics,
+                                                               metrics)
+        raise ValueError(msg)
+
+    if not end:
+        end = '{0}-{1}-{2}'.format(datetime.now().year, datetime.now().month,
+                                   datetime.now().day)
+
+    index = pd.date_range(datetime.strptime(start,"%Y-%m-%d"),
+                          datetime.strptime(end, "%Y-%m-%d"), freq='D')
+    columns = pd.MultiIndex.from_product([stocks, metrics])
+
+    data = pd.DataFrame(columns=columns, index=index)
+
+    for share in stocks:
+        for metric in metrics:
+            data.loc[:, (share, metric)] = pdr.DataReader(share,
+                                                          data_source=source,
+                                                          start=start, end=end)\
+                [metric]
+
+    data.dropna(how="all", inplace=True)
+    data.sort_index(ascending=True, inplace=True)
+
+    return data
+
+
+if __name__ == "__main__":
+
+    portfolio = ["WES", 'QBE']
+
+    candidates = ['CUV', 'ALT', "AEF", "AGL", "COH", "BHP", "ALU", "ORG", "CSL",
+                  "VAS", "WOW", "A2M", "MVF"]
+
+    portfolio.extend(candidates)
+    portfolio = [item + ".AX" for item in portfolio]
+
+    data = get_data(portfolio)
+
+    print(data.head())
+    print(data.tail())
+
