831e7a4ecbf5a6365b5d7351224269b76e9cd025
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2019-04-08 21:18:51 +1000

    [main.py]
    * Add post_processing to get shares that should be traded

--- src/main.py
+++ src/main.py
@@ -1,7 +1,30 @@
+import os
+
+from datetime import datetime
+
 import src.data.make_dataset as md
 import src.features.build_features as bf
+import src.base as base
+
+def post_processing(data):
+
+    latest_day = max(data.index)
+    mask = data.index == latest_day
+
+    columns = [feature for feature in data.columns.values if feature[1] ==
+               "Trade"]
+
+    subset = data.loc[mask, columns].T
+
+    mask = subset != "Hold"
+
+    trade_shares = subset[mask].dropna()
+
+    return trade_shares
 
 if __name__ == "__main__":
+
+    paths = base.get_paths()
 
     portfolio = ["WES", 'QBE']
 
@@ -14,4 +37,11 @@ if __name__ == "__main__":
 
     data = bf.get_trade_points(data)
 
-    print(data)
+    filename = str(datetime.now().date()) + ".csv"
+    full_filename = os.path.join(paths.get("data").get("processed"), filename)
+
+    data.to_csv(full_filename)
+
+    trade_shares = post_processing(data)
+
+    print(trade_shares)
