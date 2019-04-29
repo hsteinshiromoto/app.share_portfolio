0a78b01cec029684055a986ba4b09e697b7b8544
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2019-04-29 21:18:23 +1000

    Add tests to the output. Remove unncessary lines of code

--- src/tests/test_make_dataset.py
+++ src/tests/test_make_dataset.py
@@ -1,4 +1,5 @@
 import unittest
+import pandas as pd
 
 from src.base import get_paths, get_file
 from src.data import make_dataset as md
@@ -14,9 +15,11 @@ class TestGetData(unittest.TestCase):
 
         cls.portfolio.extend(candidates)
 
+
     @classmethod
     def tearDownClass(cls):
         cls.portfolio
+
 
     def test_raise_ValueError(self):
         with self.assertRaises(Exception):
@@ -30,20 +33,10 @@ class TestGetData(unittest.TestCase):
                 raise Exception
 
 
-    def test_upper(self):
-        print(self.portfolio)
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_Output(self):
+        data = md.get_data(self.portfolio, "yahoo")
+        self.assertIsInstance(data, pd.DataFrame)
+        self.assertEqual(data.shape[1], len(self.portfolio))
 
 if __name__ == "__main__":
     unittest.main()
