9dae9f235401f5c4b340f153a7e53569b3018474
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2019-05-17 20:30:24 +1000

    run_schedule.sh: improve auto scheduler

--- run_scheduler.sh
+++ run_scheduler.sh
@@ -5,18 +5,13 @@ repo_name=$(git rev-parse --show-toplevel)
 repo_name=$(basename ${repo_name})
 registry=registry.gitlab.com
 tag=latest
-
-if [ -z "$1" ]
-then
-	docker_image=${registry}/${user_name}/${repo_name}:${tag}
-
-else
-	docker_image=$1
-
-fi
-
+docker_image=${registry}/${user_name}/${repo_name}:${tag}
 container_id=$(docker ps -qf "ancestor=${docker_image}")
 
-
+echo "Activating cron ..."
 docker exec -u root -i ${container_id} sh -c "cron"
+echo "Done"
+
+echo "Adding taks to crontab ..."
 docker exec -i ${container_id} sh -c "cat schedule | crontab -"
+echo "Done"
