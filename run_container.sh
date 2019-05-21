806e416daaed01f12873e17d5f7da702a9d3c2ea
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2019-05-21 20:32:05 +1000

    Use double square brackets to avoid unary issues

--- run_container.sh
+++ run_container.sh
@@ -22,16 +22,20 @@ docker_image=${registry}/${user_name}/${repo_name}:${tag}
 
 container_id=$(docker ps -qf "ancestor=${docker_image}")
 
-if [ -z "$container_id" ] && [ $1 = "local" ]; then
+if [[ -z "$container_id" ]] && [[ $1 = "local" ]]; then
 	echo "Creating container from image ${docker_image}"
 	docker run -d -P -v ${h_folder_code}:${d_folder_code} -t ${docker_image}
 	container_id=$(docker ps -qf "ancestor=${docker_image}")
 
-elif [ -z "$container_id" ]
+elif [[ -z "$container_id" ]]
 then 
 	echo "Creating container from image ${docker_image}"
 	docker run -d -p 80:5000 -t ${docker_image}
 	container_id=$(docker ps -qf "ancestor=${docker_image}")
+
+	# echo "Starting Jupyter server ..."
+	# docker exec -i ${container_id} sh -c "jupyter notebook --no-browser --ip=0.0.0.0 --port=8888"
+	# echo "Done"
 
 else
 	echo "Container already running"
@@ -40,8 +44,8 @@ fi
 	
 port1=$(docker ps -f "ancestor=${docker_image}" | grep -o "0.0.0.0:[0-9]*->[0-9]*" | cut -d ":" -f 2 | head -n 1)
 port2=$(docker ps -f "ancestor=${docker_image}" | grep -o "0.0.0.0:[0-9]*->[0-9]*" | cut -d ":" -f 2 | sed -n 2p)
-token=$(docker exec -i ${container_id} sh -c "jupyter notebook list" | tac | grep -o "token=[a-z0-9]*" | sed -n 1p | cut -d "=" -f 2)
+# token=$(docker exec -i ${container_id} sh -c "jupyter notebook list" | tac | grep -o "token=[a-z0-9]*" | sed -n 1p | cut -d "=" -f 2)
 
 echo "Container ID: ${RED}${container_id}${NC}"
 echo "Port mappings: ${BLUE}${port1}, ${port2}${NC}"
-echo "Jupyter token: ${GREEN}${token}${NC}"
+# echo "Jupyter token: ${GREEN}${token}${NC}"
