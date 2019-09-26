2734a6d00cdab6b880a1ece66c7b306c1b5fc86d
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2019-09-26 22:09:23 +1000

    * Improve run_container
    * Add make_variables

--- run_container.sh
+++ run_container.sh
@@ -1,31 +1,74 @@
-#!/bin/sh
+#!/bin/bash
 
-user_name=hsteinshiromoto
-# repo_name=$(git rev-parse --show-toplevel)
-# repo_name=$(basename ${repo_name})
-repo_name=app.share_portfolio
-registry=registry.gitlab.com
-tag=latest
+# ---
+# Documentation
+# ---
 
-RED='\033[1;31m'
+display_help() {
+    echo "Usage: [variable=value] $0" >&2
+    echo
+    echo "   -h, --help                 display help"
+    echo "   -j, --jupyter_notebook     launch container with jupyter notebook"
+    echo
+    # echo some stuff here for the -a or --add-options
+    exit 1
+}
+
+jupyter() {
+    echo "Starting Jupyter Notebook"
+    DOCKER_TAG=jupyter
+}
+
+while :
+do
+    case "$1" in
+      -h | --help)
+          display_help  # Call your function
+          exit 0
+          ;;
+
+      -j | --jupyter_notebook)
+          jupyter  # Call your function
+          break
+          ;;
+
+      --) # End of all options
+          shift
+          break
+          ;;
+      -*)
+          echo "Error: Unknown option: $1" >&2
+          ## or call function display_help
+          exit 1
+          ;;
+      *)  # No more options
+          break
+          ;;
+    esac
+done
+
+# ---
+# Get Variables From make_variables.sh
+# ---
+IFS='|| ' read -r -a array <<< $(./make_variables.sh)
+
+DOCKER_IMAGE=${array[0]}
+PROJECT_NAME=${array[1]}
+DOCKER_TAG="${DOCKER_TAG:-latest}"
+DOCKER_IMAGE_TAG=${DOCKER_IMAGE}:${DOCKER_TAG}
+
+CONTAINER_ID=$(docker ps | grep "${DOCKER_IMAGE_TAG}" | awk '{ print $1}')
+
+RED="\033[1;31m"
 BLUE='\033[1;34m'
 GREEN='\033[1;32m'
 NC='\033[0m'
 
-# Host folders
-h_folder_code=$(pwd)
 
-# Docker folders
-d_folder_code=/home/${repo_name}
-
-docker_image=${registry}/${user_name}/${repo_name}:${tag}
-
-container_id=$(docker ps -qf "ancestor=${docker_image}")
-
-if [ -z "$container_id" ] && [ $1 = "local" ]; then
-	echo "Creating container from image ${docker_image}"
-	docker run -d -P -v ${h_folder_code}:${d_folder_code} -t ${docker_image}
-	container_id=$(docker ps -qf "ancestor=${docker_image}")
+if [[ -z "${CONTAINER_ID}" ]]; then
+	echo "Creating Container from Image ${DOCKER_IMAGE_TAG}"
+	docker run -d -P -v $(pwd):/home/${PROJECT_NAME} -t ${DOCKER_IMAGE_TAG}
+	CONTAINER_ID=$(docker ps | grep "${DOCKER_IMAGE_TAG}" | awk '{ print $1}')
 
 elif [ $1 = "deploy" ]
 then 
