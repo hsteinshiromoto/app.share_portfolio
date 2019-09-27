8dda04d3f5c36de63833181e69fb6e04e20b917d
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2019-09-27 20:20:32 +1000

    * Improve entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,10 @@ set -e
 
 # If "-e uid={custom/local user id}" flag is not set for "docker run" command, use 9999 as default
 CURRENT_UID=${uid:-9999}
+DOCKER_USER=${DOCKER_USER:-docker_user}
 
 # Create user called "docker" with selected UID
-useradd --shell /bin/bash -u $CURRENT_UID -o -c "" -m $USER
+useradd --shell /bin/bash -u $CURRENT_UID -o -c "" -m $DOCKER_USER
 
 # Execute process
-exec gosu $USER "$@"
+exec gosu $DOCKER_USER "$@"
