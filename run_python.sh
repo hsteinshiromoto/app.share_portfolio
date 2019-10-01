2776974b1c93e0314cc594f0b1794d68e8282a3e
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2019-10-01 20:13:06 +1000

    [run_python.sh]
    Start adding functions

--- run_python.sh
+++ run_python.sh
@@ -26,3 +26,67 @@ if [[ $1 = "clean" ]]; then
 	find . -type f -name "*.py[co]" -delete
 	find . -type d -name "__pycache__" -delete
 fi
+
+# Documentation
+display_help() {
+    echo "Usage: [variable=value] $0" >&2
+    echo
+    echo "   -c, --clean                Remove files *.py[co] and __pycache__"
+    echo "   -m, --make_dataset         Run make_dataset.py"
+    echo "   -h, --help                 Display help"
+    echo "   -r, --requirements         Install modules from requirements.txt"
+    echo "   -t, --test_environment     Test python environment"
+    echo
+    # echo some stuff here for the -a or --add-options
+    exit 1
+}
+
+
+# Available options
+while :
+do
+    case "$1" in
+      -h | --help)
+          display_help  # Call your function
+          exit 0
+          ;;
+
+      -c | --clean)
+          clean  # Call your function
+          break
+          ;;
+
+      -m | --make_dataset)
+          jupyter  # Call your function
+          break
+          ;;
+
+      -r | --requirements)
+          deploy_container  # Call your function
+          break
+          ;;
+
+      -t | --test_environment)
+          run_ssh_container  # Call your function
+          break
+          ;;
+
+      "")
+          run_container  # Call your function
+          break
+          ;;
+
+      --) # End of all options
+          shift
+          break
+          ;;
+      -*)
+          echo "Error: Unknown option: $1" >&2
+          ## or call function display_help
+          exit 1
+          ;;
+      *)  # No more options
+          break
+          ;;
+    esac
+done
